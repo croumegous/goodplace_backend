e2616b2a9a4c7203b0c4e7e3d5bd3b9370d41cec
Mishozu <cyril.roumegous@gmail.com>
2021-01-27 13:42:26 +0100

    fix: fix pytest with new authorizations

--- tests/schemathesis/test_schemathesis.py
+++ tests/schemathesis/test_schemathesis.py
@@ -3,21 +3,24 @@ import schemathesis
 from schemathesis.checks import DEFAULT_CHECKS
 
 from good_place.main import app
+from tests.fixtures.db import client
 
 # Schemathesis is a modern API testing tool for web applications see : https://github.com/schemathesis/schemathesis
 
 schema = schemathesis.from_asgi("/openapi.json", app)
 
 
-# def test_schemathesis(client, case):
-#     res = client.request(
-#             case.method, case.formatted_path, headers=case.headers
-#         )
-#     case.validate_response(res, DEFAULT_CHECKS)
-
-
 @pytest.mark.schemathesis
 @schema.parametrize()
-def test_schemathesis(case):
-    res = case.call_asgi()
+def test_schemathesis(client, case):
+    res = client.request(case.method, case.formatted_path, headers=case.headers)
     case.validate_response(res, DEFAULT_CHECKS)
+
+
+# schema = schemathesis.from_asgi("/openapi.json", app)
+
+# @pytest.mark.schemathesis
+# @schema.parametrize()
+# def test_schemathesis(case):
+#     res = case.call_asgi()
+#     case.validate_response(res, DEFAULT_CHECKS)
